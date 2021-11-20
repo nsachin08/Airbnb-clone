a93d5ec3ad5bf7ffbdf302e8d896eea37e878f4b
nsachin08 <naiksci90@gmail.com>
2021-11-21 01:56:30 +0530

    Edited on 21 Nov 2021

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__icon"
+        src="https://i.pinimg.com/originals/1b/20/58/1b20588ad588cf694ce7af9197f3722d.jpg"
+        alt=""
+      />
+
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Banner from "./Banner.js";
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Home component</h1>
+      <Banner />
+    </div>
+  );
+}
+
+export default Home;
